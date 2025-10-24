33ede77dd33aabcf1aaa5b0fe9dbf0766b4dceca
Ignacio W <ignaciowuilloud@gmail.com>
2025-10-24 20:54:45 -0300

    Servidor simple

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func (c *gin.Context) {
+		// c.String(200, "Hola mundo")
+		c.JSON(200, gin.H{
+			"message":"Hola mundillo",
+		})
+	})
+	r.Run(":8080")
+}
